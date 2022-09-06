5f46b434fe0f72dad6e9d13ccb3b497e71962114
norgeous <daniel@harte.me>
2022-09-06 20:57:13 +0100

    switch to matterjs

--- src/hooks/usePhaser.js
+++ src/hooks/usePhaser.js
@@ -1,37 +1,37 @@
 import { useState, useEffect } from 'react';
 import Phaser from 'phaser';
-// import getConfig from '../phaser/scene1';
 import Game from '../phaser/Game';
+
+const config = {
+  type: Phaser.AUTO,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    parent: 'phaser',
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: 16 * 50,
+    height: 9 * 50,
+  },
+  pixelArt: true,
+  antialias: false,
+  autoRound: true,
+  roundPixels: true,
+  physics: {
+    default: 'matter',
+    matter: {
+      // debug: true,
+      // gravity: { y: 200 }
+    }
+  },
+  scene: [Game],
+};
 
 const usePhaser = () => {
   const [score, setScore] = useState(0);
   const [game, setGame] = useState();
 
   useEffect(() => {
-    // const config = getConfig({ setScore });
-    // setGame(new Phaser.Game(config));
-    setGame(new Phaser.Game({
-      type: Phaser.AUTO,
-      scale: {
-        mode: Phaser.Scale.FIT,
-        parent: 'phaser',
-        autoCenter: Phaser.Scale.CENTER_BOTH,
-        width: 16 * 50,
-        height: 9 * 50,
-      },
-      pixelArt: true,
-      antialias: false,
-      autoRound: true,
-      roundPixels: true,
-      physics: {
-        default: 'arcade',
-        arcade: {
-          // debug: true,
-          // gravity: { y: 200 }
-        }
-      },
-      scene: [Game],
-    }));
+    const newGame = new Phaser.Game(config);
+    setGame(newGame);
   }, []);
 
   return { score, game };
--- src/phaser/Game.js
+++ src/phaser/Game.js
@@ -8,12 +8,22 @@ class Game extends Phaser.Scene {
 
   preload () {
     this.load.setBaseURL('https://labs.phaser.io');
+    this.load.image('red', 'assets/particles/fire1.png');
     // this.load.image('sky', 'assets/skies/space3.png');
     // this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-    this.load.image('red', 'assets/particles/fire1.png');
   }
 
   create () {
+    const { width, height } = this.sys.game.canvas;
+
+    this.matter.world.disableGravity();
+    this.matter.world.setBounds(0, 0, width, height, 15);
+    this.cameras.main.setBounds(0, 0, width, height);
+
+
+    // this.cameras.main.setZoom(1.1);
+    this.cameras.main.centerOn(width/2, height);
+
     // this.add.image(width/2, height/2, 'sky');
     const particles = this.add.particles('red');
     const emitter = particles.createEmitter({
@@ -22,38 +32,72 @@ class Game extends Phaser.Scene {
       blendMode: 'ADD'
     });
 
-    const { width, height } = this.sys.game.canvas;
 
-    const logo = this.add.text(width/2, height/4, '🥴', { font: '50px Arial', align: 'center' }).setOrigin(0.5);
-    this.physics.world.enable(logo);
-    logo.body.setCircle(26, 5);
-    logo.body.setBounce(1, 1);
-    logo.body.setVelocity(100, -200);
-    logo.body.setMaxVelocity(500);
-    logo.body.collideWorldBounds = true;
-    logo.body.onWorldBounds = true;
-    emitter.startFollow(logo);
 
-    const player = this.add.rectangle(
-      this.physics.world.bounds.width/2,
-      this.physics.world.bounds.height - 25,
-      150, 20,
-      0x6666ff,
-    );    
-    this.physics.add.existing(player);
-    player.name = 'player';
-    player.body.setBounce(1, 1);
-    player.body.setImmovable();
-    player.body.setAllowGravity(false);
-    // player.body.setGravity(false);
-    player.body.collideWorldBounds = true;
 
-    this.physics.add.collider(player, logo);
+
+    const ball = this.add.text(width/2, height/4, '🥴', { font: '50px Arial', align: 'center' }).setOrigin(0.5);
+    emitter.startFollow(ball);
+    // this.physics.world.enable(ball);
+    // ball.body.setCircle(26, 5);
+    // ball.body.setBounce(1, 1);
+    // ball.body.setVelocity(100, -200);
+    // ball.body.setMaxVelocity(500);
+    // ball.body.collideWorldBounds = true;
+    // ball.body.onWorldBounds = true;
+
+    this.ball = this.matter.add.gameObject(ball, { shape: { type: 'circle', radius: 26 }});
+    this.ball
+      .setFrictionAir(0)
+      .setBounce(1)
+      .setVelocity(20);
+
+
+    // const matterText = this.matter.add.gameObject(ball, { shape: { type: 'circle', radius: 64 } })
+    //   .setFrictionAir(0.001)
+    //   .setBounce(0.9)
+    //   .setCollideWorldBounds(true);
+    // var matterText2 = this.matter.add.gameObject(text2).setFrictionAir(0.001).setBounce(0.9);
+
+
+
+    // const player = this.add.rectangle(
+    //   this.physics.world.bounds.width/2,
+    //   this.physics.world.bounds.height - 25,
+    //   150, 20,
+    //   0x6666ff,
+    // );    
+    // this.physics.add.existing(player);
+    // player.name = 'player';
+    // // player.body.setBounce(1, 1);
+    // player.body.setImmovable();
+    // player.body.setAllowGravity(false);
+    // // player.body.setGravity(false);
+    // player.body.collideWorldBounds = true;
+
+
+
+
+
+    // this.physics.add.collider(player, ball);
+    // this.cameras.main.startFollow(player, false, 0.2, 0.2);
 
     // this.physics.world.on('worldbounds', thing => {
     //   // console.log(thing);
     //   setScore(s => s + 1);
     // });
+  }
+
+  update () {
+    // console.log(this.ball.body.velocity)
+
+    // ball min speed
+    const { x, y } = this.ball.body.velocity;
+    if(x>0 && x<5) this.ball.setVelocityX(5);
+    if(x<0 && x>-5) this.ball.setVelocityX(-5);
+    if(y>0 && y<5) this.ball.setVelocityY(5);
+    if(y<0 && y>-5) this.ball.setVelocityY(-5);
+
   }
 }
 
