44c54b6dc3f0ded3116db4c1e9456df307eeb023
norgeous <daniel@harte.me>
2022-09-10 09:13:02 +0100

    Update gameScene.js

--- src/phaser/gameScene.js
+++ src/phaser/gameScene.js
@@ -2,7 +2,7 @@
 
 export function preload () {
   this.load.setBaseURL('https://labs.phaser.io');
-  this.load.image('red', 'assets/particles/fire1.png');
+  this.load.image('red', 'assets/particles/red.png');
   // this.load.image('sky', 'assets/skies/space3.png');
   // this.load.image('logo', 'assets/sprites/phaser3-logo.png');
 }
