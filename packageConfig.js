b900cb1315010df6bef215783e8fb954368e617c
norgeous <daniel@harte.me>
2022-09-18 10:19:58 +0100

    Update packageConfig.js

--- packageConfig.js
+++ packageConfig.js
@@ -1,5 +1,5 @@
 globalThis.packageConfig = {
   name: 'polygon-pong',
-  version: '0.0.0', // increase to invalidate sw's old caches on clientside
+  version: '0.0.1', // increase to invalidate service worker's cache on clientside
   repository: 'https://github.com/norgeous/polygon-pong/',
 };
