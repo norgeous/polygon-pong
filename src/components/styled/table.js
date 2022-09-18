fa45d31528fbd4f04e25f16acdc0b3373159cfcb
norgeous <daniel@harte.me>
2022-09-18 11:19:58 +0100

    host tag

--- src/components/styled/table.js
+++ src/components/styled/table.js
@@ -3,6 +3,7 @@ import styled, { css } from 'styled-components';
 export const Table = styled.table`
   padding: 10px;
   background: #3037;
+  white-space: nowrap;
 `;
 export const Tr = styled.tr``;
 export const Td = styled.td`
