96371340c913c4f918abc93d09099cd9389aad06
norgeous <daniel@harte.me>
2022-09-18 14:25:18 +0100

    Update useBattery.js

--- src/hooks/useBattery.js
+++ src/hooks/useBattery.js
@@ -1,21 +1,28 @@
 import { useState, useEffect } from 'react';
 
+const available = 'getBattery' in navigator;
+
 const useBattery = () => {
   const [battery, setBattery] = useState();
 
   useEffect(() => {
-    const getBattery = async () => {
-      const newBattery = await navigator.getBattery?.();
-      setBattery(newBattery);
-    };
-    getBattery();
-    const t = setInterval(getBattery, 10*1000);
-    return () => clearInterval(t);
+    if (available) {
+      const getBattery = async () => {
+        const newBattery = await navigator.getBattery();
+        setBattery(newBattery);
+      };
+
+      getBattery();
+      
+      const t = setInterval(getBattery, 10 * 1000); // poll every 10 seconds
+      
+      return () => clearInterval(t);
+    }
   }, []);
 
-  const batteryPercent = Math.floor((battery?.level || 0) * 100);
+  const batteryPercent = `${Math.floor((battery?.level || 0) * 100)}%`;
 
-  return batteryPercent;
+  return [available, batteryPercent];
 };
 
 export default useBattery;
