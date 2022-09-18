f4dd35ce1cfd1210a5b7f1c2796a5827eaecad1d
norgeous <daniel@harte.me>
2022-09-18 12:53:51 +0100

    refactor useLocalStorage

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,14 +1,16 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const useLocalStorage = (key, defaultValue) => {
-  const [value, setValue] = useState(JSON.parse(localStorage.getItem(key)) || defaultValue);
+  const item = localStorage.getItem(key);
+  const [value, setValue] = useState(item && JSON.parse(item) || defaultValue);
 
-  const setValueLocalStorage = (newValue) => {
-    setValue(newValue);
-    localStorage.setItem(key, JSON.stringify(newValue));
-  };
+  // update local storage when value changes
+  useEffect(() => {
+    if (value) localStorage.setItem(key, JSON.stringify(value));
+    else localStorage.removeItem(key);
+  }, [value]);
 
-  return [value, setValueLocalStorage];
+  return [value, setValue];
 };
 
 export default useLocalStorage;
