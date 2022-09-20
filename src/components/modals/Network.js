bb0c782a47e5a2ac7d379d86c4d0037e514cd538
norgeous <daniel@harte.me>
2022-09-21 00:49:21 +0100

    Update Network.js

--- src/components/modals/Network.js
+++ src/components/modals/Network.js
@@ -12,51 +12,53 @@ const Network = () => {
     hostFitness,
     peerIds, peerId, connections, peerData,
     sysInfo,
+    peerNet,
   } = useAppContext();
 
-  const peerList = peerIds.map(id => {
-    if(id === peerId) return {
-      id,
-      status: 'SELF',
-      location,
-      platform: navigator.platform,
-      hostFitness,
-    };
+  // const peerList = peerIds.map(id => {
+  //   if(id === peerId) return {
+  //     id,
+  //     status: 'SELF',
+  //     location,
+  //     platform: navigator.platform,
+  //     hostFitness,
+  //   };
           
-    const conn = connections.find(conn => conn.peer === id);
+  //   const conn = connections.find(conn => conn.peer === id);
 
-    if (conn) {
-      const pd = peerData[conn.peer];
-      return {
-        id,
-        status: 'CONNECTED',
-        ...pd,
-      };
-    }
+  //   if (conn) {
+  //     const pd = peerData[conn.peer];
+  //     return {
+  //       id,
+  //       status: 'CONNECTED',
+  //       ...pd,
+  //     };
+  //   }
 
-    return {
-      id,
-      status: 'DISCONNECTED',
-    };
-  });
+  //   return {
+  //     id,
+  //     status: 'DISCONNECTED',
+  //   };
+  // });
 
-  const whoIsHost = peerList.reduce((a, b) => {
-    if (b.hostFitness === undefined) return a;
-    if (a.hostFitness < b.hostFitness) return a;
-    else return b;
-  }, {});
+  // const whoIsHost = peerList.reduce((a, b) => {
+  //   if (b.hostFitness === undefined) return a;
+  //   if (a.hostFitness < b.hostFitness) return a;
+  //   else return b;
+  // }, {});
 
   return (
     <Modal
       title={`${getUiIcon('network')} Network`}
       onClose={() => setRoute()}
     >
-      {!peerList.length && `${getUiIcon('disconnected')} No Connections`}
+      {/* {!peerList.length && `${getUiIcon('disconnected')} No Connections`}
       {!!peerList.length && (
         <Table>
           {peerList.map(p => p.id === peerId ?<SystemInfo {...sysInfo} {...p} />:<SystemInfo isHost={whoIsHost.id === p.id} {...p} />)}
         </Table>
-      )}
+      )} */}
+      <pre>{JSON.stringify(peerNet.connections,null,2)}</pre>
     </Modal>
   );
 };
